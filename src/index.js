dad223650edb98aae20b2f745c0ed782079b01e7
Laboratoria <me@claudiagarfias.works>
2019-02-03 14:34:19 -0600

    A esta versión 0,4 solamente le falta editar el readme y la función para saltar los espacios en los input-messages y así poder encode y decode frases en lugar de sólo palabras

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 
+const startApp = document.getElementById('startApp');
+	startApp.addEventListener('click'	, () => {
+	location.replace('app.html');
+});
+
+//APRENDIZAJE DE querySelector
+//la siguiente forma de programar botones usa querySelector, como todavía no descubro la diferencia 
+//entre esto y lo que usé para programar otros botones, la dejaré porque sé que me servirá 
+//para aprender posteriormente...
 const  encodeBttn = () => {
-	let resultEncode = cipher.encode(parseInt(document.getElementById('keyEncrypt').value),document.getElementById('messageEncrypt').value);
+	let resultEncode = cipher.encode(parseInt(document.getElementById('keyEncrypt').value),document.getElementById('messageEncrypt').value.toUpperCase());
 	document.getElementById('resultEncrypt').innerHTML = resultEncode; //envía el resultado al p element resulEncrypt
 	const encryptScreen = document.querySelector('#encryptScreen');
 	encryptScreen.style.display = 'none';
 	const showOutput = document.querySelector('#outputScreen');
 	showOutput.style.display = 'block';
 	return resultEncode;
-
-
 };
 document.getElementById("encryptButton").addEventListener('click',encodeBttn);
 
 const  decodeBttn = () => {
-	let resultDecode = cipher.decode(parseInt(document.getElementById('keyDecrypt').value),document.getElementById('messageDecrypt').value);
+	let resultDecode = cipher.decode(parseInt(document.getElementById('keyDecrypt').value),document.getElementById('messageDecrypt').value.toUpperCase());
 	document.getElementById('resultDecrypt').innerHTML = resultDecode; //envía el resultado al p element resulEncrypt
 	const decryptScreen = document.querySelector('#decryptScreen');
 	decryptScreen.style.display = 'none';
@@ -23,22 +30,13 @@ const  decodeBttn = () => {
 };
 document.getElementById('decryptButton').addEventListener('click',decodeBttn);
 
-const startApp = document.getElementById('startApp');
-startApp.addEventListener('click'	, () => {
-	const explanationScreen = document.querySelector('#explanationScreen');
-	explanationScreen.style.display = 'none';
-	const showChoose = document.querySelector('#chooseScreen');
-	showChoose.style.display = 'block';
-	});
-
-const btnEncrypt = document.getElementById('chooseEncrypt');
+const btnEncrypt = document.getElementById('chooseEncrypt'); 
 btnEncrypt.addEventListener('click'	, () => {
 	const chooseScreen = document.querySelector('#chooseScreen');
 	chooseScreen.style.display = 'none';
 	const showEncode = document.querySelector('#encryptScreen');
 	showEncode.style.display = 'block';
 	});
-
 
 const btnDecrypt = document.getElementById('chooseDecrypt');
 btnDecrypt.addEventListener('click'	, () => {
@@ -47,13 +45,14 @@ btnDecrypt.addEventListener('click'	, () => {
 	const showDecode = document.querySelector('#decryptScreen');
 	showDecode.style.display = 'block';
 	});
+// TERMINA APRENDIZAJE de querySelector
 
 const btnAgain = document.getElementById('againButton');
 btnAgain.addEventListener('click', ()=>{
 	location.reload(true);
 });
 
-// const btnQuit = document.getElementById('quitButton');
-// btnAgain.addEventListener('click', ()=>{
-// 	location.replace('index.html').reload(true);
-// });
+const reloadBttn = document.getElementById('quitButton');
+	reloadBttn.addEventListener('click'	, () => {
+	location.replace('index.html');
+});
