1d51f2b623dd5c93d80c1dd5172d9f900033869e
Laboratoria <me@claudiagarfias.works>
2019-02-07 12:27:25 -0600

    Ya pasa la prueba de Hacker Edition pero no cifra todos los caracteres deseados, como ñ, acento grave y otros. Para probar hacker hay que descomentar el cipher.spec.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,65 @@
-
-
-const encode = (offset,string) => {
-	const stringCiphered = []; //array para almacenar los caracteres cifrados
+const encode = (offset,string) => { // ARGUMENTOS
+	const stringCiph = []; //RESULTADO EN UN ARRAY
 	
-	for (let i = 0; i < string.length; i++) { //contador para obtener cada letra del string
-		let character = string.charCodeAt(i); // cada letra del string en código ASCII
-		let characterCiphered = (character - 65 + offset) % 26 + 65; //cifra cada letra del string usando el offset dado por el usuario y devuelve su código ASCII
-		stringCiphered.push(String.fromCharCode(characterCiphered)); // envía al array los caracteres cifrados en ASCII
-	}
-	const output = stringCiphered.join(''); //une los caracteres del array y los almacena en output
-	return output;
-};
+	for (let i = 0; i < string.length; i++) { //BUCLE PARA ITERAR LOS CARACTERES
 
+		let charIdx = string.charCodeAt(i);//OBTIENE EL CODIGO ASCII
 
-const decode = (offset,string) => {
-	const stringDeciphered = []; //array para almacenar los caracteres cifrados
+		if (charIdx >= 32 && charIdx <= 64){ //PASA SIN CIFRAR ENTRE CARACTERES 32 Y 64 DEL ASCII QUE SON ALGUNOS ESPECIALES
+		stringCiph.push(String.fromCharCode(charIdx));
+		}
+	//PARTE OBLIGATORIA AQUÍ ENCODE
+		else if (charIdx >= 65 && charIdx <= 90){ //CIFRA ENTRE CARACTERES 65 Y 90 DEL ASCII
+		let charCiph = (charIdx - 65 + offset) % 26 + 65; //FORMULA DE MICHELLE
+		stringCiph.push(String.fromCharCode(charCiph)); //EMPUJA CADA CHAR CIFRADO AL ARRAY DEL RESULTADO
+		}
+
+		else if (charIdx >= 97 && charIdx <= 122){ //CIFRA MINÚSCULAS
+		let charCiph = (charIdx - 97 + offset) % 6 + 97;
+		stringCiph.push(String.fromCharCode(charCiph));
+
+		}
+
+		else { //RETORNA IGUAL CUALQUIERA QUE NO ENTRE EN EL RANGO DE LOS ANTERIORES
+		return stringCiph.push(String.fromCharCode(charIdx));
+		}
+
+		}
 	
-	for (let i = 0; i < string.length; i++) { //contador para obtener cada letra del string
-		let character = string.charCodeAt(i); // cada letra del string en código ASCII
-		let characterDeciphered = (character + 65 - offset) % 26 + 65; //cifra cada letra del string usando el offset dado por el usuario y devuelve su código ASCII
-		stringDeciphered.push(String.fromCharCode(characterDeciphered)); // envía al array los caracteres cifrados en ASCII
-	}
-	const output = stringDeciphered.join(''); //une los caracteres del array y los almacena en output
-	return output;
-};
+		return stringCiph.join(''); 
+	};
+	
 
+const decode = (offset,string) => { // ARGUMENTOS
+	const stringDeci = [];//RESULTADO EN UN ARRAY
+
+	for (let i = 0; i < string.length; i++) { //BUCLE PARA ITERAR LOS CARACTERES
+
+		let charIdx = string.charCodeAt(i); //OBTIENE EL CODIGO ASCII
+
+		if (charIdx >= 32 && charIdx <= 64){//PASA SIN DESCIFRAR ENTRE CARACTERES 32 Y 64 DEL ASCII QUE SON ALGUNOS ESPECIALES
+		stringDeci.push(String.fromCharCode(charIdx));
+		}
+	//PARTE OBLIGATORIA AQUI DECODE
+		else if (charIdx >= 65 && charIdx <= 90){
+		let charDeci = (charIdx + 65 - offset) % 26 + 65;
+		stringDeci.push(String.fromCharCode(charDeci));
+		}
+
+		else if(charIdx >= 97 && charIdx <= 122){//DESCIFRA MINÚSCULAS
+			let charDeci = (charIdx + 59 - offset) % 26 + 97;
+			stringDeci.push(String.fromCharCode(charDeci));
+		}
+	
+		else {//RETORNA IGUAL CUALQUIERA QUE NO ENTRE EN EL RANGO DE LOS ANTERIORES
+		return stringDeci.push(String.fromCharCode(charIdx));
+		}
+	}
+	
+	return stringDeci.join(''); 
+};
 
 window.cipher = {
 	encode: encode,
 	decode: decode,
-  // ... 
 };
-
-
