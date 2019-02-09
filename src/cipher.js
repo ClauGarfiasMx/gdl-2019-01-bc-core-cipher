900dbec3247e869cbf60e9f9cd3c960016416d1a
Laboratoria <me@claudiagarfias.works>
2019-02-08 20:08:04 -0600

    Los archivos se cargaron correctamente

--- src/cipher.js
+++ src/cipher.js
@@ -8,7 +8,7 @@ const encode = (offset,string) => { // ARGUMENTOS
 		if (charIdx >= 32 && charIdx <= 64){ //PASA SIN CIFRAR ENTRE CARACTERES 32 Y 64 DEL ASCII QUE SON ALGUNOS ESPECIALES
 		stringCiph.push(String.fromCharCode(charIdx));
 		}
-	//PARTE OBLIGATORIA AQUÍ: ENCODE
+	//PARTE OBLIGATORIA AQUÍ ENCODE
 		else if (charIdx >= 65 && charIdx <= 90){ //CIFRA ENTRE CARACTERES 65 Y 90 DEL ASCII
 		let charCiph = (charIdx - 65 + offset) % 26 + 65; //FORMULA DE MICHELLE
 		stringCiph.push(String.fromCharCode(charCiph)); //EMPUJA CADA CHAR CIFRADO AL ARRAY DEL RESULTADO
@@ -40,7 +40,7 @@ const decode = (offset,string) => { // ARGUMENTOS
 		if (charIdx >= 32 && charIdx <= 64){//PASA SIN DESCIFRAR ENTRE CARACTERES 32 Y 64 DEL ASCII QUE SON ALGUNOS ESPECIALES
 		stringDeci.push(String.fromCharCode(charIdx));
 		}
-	//PARTE OBLIGATORIA AQUI: DECODE
+	//PARTE OBLIGATORIA AQUI DECODE
 		else if (charIdx >= 65 && charIdx <= 90){
 		let charDeci = (charIdx + 65 - offset) % 26 + 65;
 		stringDeci.push(String.fromCharCode(charDeci));
